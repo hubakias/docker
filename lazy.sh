e2a0bfe75219720f33a079c1050106124dd49814
Konstantinos <kostikas@gmail.com>
2015-12-17 15:20:32 +0200

    Correct an omission

--- lazy.sh
+++ lazy.sh
@@ -2,4 +2,4 @@
 for i in $(docker images | tail -n +2 | awk '{print $1}' | egrep -v "^<none>" ) ; do docker pull $i ; done
 
 # Remove everything not linked to a container (running or not)
-for in $(docker images | egrep "^<none>" | awk '{print $3}' ) ; do docker rmi $i ; done
+for i in $(docker images | egrep "^<none>" | awk '{print $3}') ; do docker rmi $i ; done
